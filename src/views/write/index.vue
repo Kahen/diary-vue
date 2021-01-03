368976b1abeb1964088f06e3b5f877069ff7568d
lijiaxing <kahenlee@163.com>
2021-01-03 22:29:43 +0800

    fix

--- src/views/write/index.vue
+++ src/views/write/index.vue
@@ -1,6 +1,5 @@
 <template>
 
-
   <div style="margin: 20px">
     <div style="background: #409EFF;height: 100px;margin-bottom: 20px"></div>
     <div>
@@ -59,16 +58,93 @@
         </el-form>
       </el-row>
     </div>
+    <div>
+      <!--      <el-button type="text" @click="dialog = true">打开嵌套 Form 的 Drawer</el-button>-->
+      <div style="width: 100%">
+        <div @click="dialog=true" style="width: 100px;height: 100px;background: #409EFF">
 
+        </div>
+        <div @click="dialog=true" style="width: 100px;height: 100px;background: #409EFF"></div>
+        <div @click="dialog=true" style="width: 100px;height: 100px;background: #409EFF"></div>
+        <div @click="dialog=true" style="width: 100px;height: 100px;background: #409EFF"></div>
+        <div @click="dialog=true" style="width: 100px;height: 100px;background: #409EFF"></div>
+        <div @click="dialog=true" style="width: 100px;height: 100px;background: #409EFF"></div>
+
+      </div>
+      <el-drawer
+        :before-close="handleClose"
+        :visible.sync="dialog"
+        custom-class="demo-drawer"
+        direction="ltr"
+        ref="drawer"
+        size="100%"
+        title="我嵌套了 Form !"
+      >
+        <div class="demo-drawer__content">
+          <el-form :model="form">
+            <el-form-item :label-width="formLabelWidth" label="活动名称">
+              <el-input autocomplete="off" v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item :label-width="formLabelWidth" label="活动区域">
+              <el-select placeholder="请选择活动区域" v-model="form.region">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-form>
+          <div class="demo-drawer__footer">
+            <el-button @click="cancelForm">取 消</el-button>
+            <el-button :loading="loading" @click="$refs.drawer.closeDrawer()" type="primary">{{ loading ? '提交中 ...' :
+              '确定' }}
+            </el-button>
+          </div>
+        </div>
+      </el-drawer>
+    </div>
   </div>
 </template>
 <script>
   export default {
+
     name: 'Write',
     components: {},
     props: [],
     data() {
       return {
+        truee: true,
+        table: false,
+        dialog: false,
+        loading: false,
+        gridData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '80px',
+        timer: null,
+
         formData: {
           field1: null,
           field112: undefined,
@@ -104,6 +180,30 @@
       },
       resetForm() {
         this.$refs['elForm'].resetFields()
+      },
+
+      handleClose(done) {
+        if (this.loading) {
+          return
+        }
+        this.$confirm('确定要提交表单吗？')
+          .then(_ => {
+            this.loading = true
+            this.timer = setTimeout(() => {
+              done()
+              // 动画关闭需要一定的时间
+              setTimeout(() => {
+                this.loading = false
+              }, 400)
+            }, 2000)
+          })
+          .catch(_ => {
+          })
+      },
+      cancelForm() {
+        this.loading = false
+        this.dialog = false
+        clearTimeout(this.timer)
       }
     }
   }
